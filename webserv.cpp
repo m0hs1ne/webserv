5b05ea4c992affc8ea417b38ce39d67f2e1b7e26
m0hs1ne <mel-hada@student.1337.ma>
2023-03-05 10:43:16 +0100

    fixed multi server

--- webserv.cpp
+++ webserv.cpp
@@ -9,7 +9,7 @@
 
 void startServers(std::vector<parsingConfig::server> servers)
 {
-    int server_fd[30], new_socket, max_sd, sd, activity,valread;
+    int server_fd[1024], new_socket, max_sd, sd, activity, valread;
     struct sockaddr_in address;
     Response res;
     int addrlen = sizeof(address);
@@ -26,6 +26,7 @@ void startServers(std::vector<parsingConfig::server> servers)
             perror("socket failed");
             exit(EXIT_FAILURE);
         }
+        fcntl(server_fd[i], F_SETFL, O_NONBLOCK);
         if (setsockopt(server_fd[i], SOL_SOCKET, SO_REUSEADDR, &valread, sizeof(valread)))
         {
             perror("setsockopt");
@@ -53,7 +54,7 @@ void startServers(std::vector<parsingConfig::server> servers)
         FD_ZERO(&readfds);
         for (size_t i = 0; i < servers.size(); i++)
             FD_SET(server_fd[i], &readfds);
-        max_sd = server_fd[0];
+        max_sd = server_fd[servers.size() - 1];
         for (size_t i = 0; i < servers.size(); i++)
         {
             sd = server_fd[i];
@@ -62,32 +63,64 @@ void startServers(std::vector<parsingConfig::server> servers)
             if (sd > max_sd)
                 max_sd = sd;
         }
-        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
-        if ((activity < 0) && (errno != EINTR))
+        if ((activity = select(max_sd + 1, &readfds, NULL, NULL, NULL)) < 0)
         {
-            std::cout << "select error" << std::endl;
+            perror("select");
+            exit(EXIT_FAILURE);
         }
+        if (activity == 0)
+        {
+            std::cout << "Timeout" << std::endl;
+            continue;
+        }
+        std::cout << "Activity " << activity << std::endl;
         for (size_t i = 0; i < servers.size(); i++)
         {
+            std::cout << "server " << i << std::endl;
             sd = server_fd[i];
             if (FD_ISSET(sd, &readfds))
             {
-                if ((new_socket = accept(sd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+                if (sd == server_fd[i])
                 {
-                    perror("accept");
-                    exit(EXIT_FAILURE);
+                    std::cout << "socket " << sd << std::endl;
+                    if ((new_socket = accept(sd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+                    {
+                        perror("accept");
+                        exit(EXIT_FAILURE);
+                    }
+                    fcntl(new_socket, F_SETFL, O_NONBLOCK);
+                    FD_SET(new_socket, &readfds);
                 }
-                std::cout << "New connection" << std::endl;
-                std::cout << "Accepted socket: " << new_socket << std::endl;
-                char buffer[MAX] = {0};
-                valread = read(new_socket, buffer, MAX -1);
-                // std::cout << buffer << std::endl;
-                if(valread > 0)
-                    res = handleRequest(buffer, servers[i]);
-                std::string hello = res.response;
-                // std::string hello = "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello world!";
-                send(new_socket, hello.c_str(), hello.length(), 0);
-                std::cout << "Hello message sent" << std::endl;
+                for (int i = 0; i < 30; i++)
+                {
+                    if (FD_ISSET(i, &readfds))
+                        std::cout << "fd " << i << std::endl;
+                }
+                if (FD_ISSET(new_socket, &readfds))
+                {
+                    std::cout << "New connection" << std::endl;
+                    std::cout << "Accepted socket: " << new_socket << std::endl;
+                    char buffer[MAX] = {0};
+                    valread = read(new_socket, buffer, MAX - 1);
+                    // std::cout << buffer << std::endl;
+                    std::cout << "valread: " << valread << std::endl;
+                    if (valread <= 0)
+                    {
+                        close(new_socket);
+                        FD_CLR(new_socket, &readfds);
+                        continue;
+                    }
+                    else
+                    {
+                        res = handleRequest(buffer, servers[i]);
+                        std::string hello = res.response;
+                        // std::string hello = "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello world!";
+                        send(new_socket, hello.c_str(), hello.length(), 0);
+                        std::cout << "Hello message sent" << std::endl;
+                        close(new_socket);
+                        FD_CLR(new_socket, &readfds);
+                    }
+                }
             }
         }
     }
@@ -107,7 +140,7 @@ int main(int ac, char **av)
         while (true)
             startServers(servers);
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
         std::cerr << e.what() << '\n';
     }
